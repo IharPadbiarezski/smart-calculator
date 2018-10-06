7793fca59e047c85e90ffc6eaabf890ba20ce0a3
Ihar Padbiarezski <yb.igrok.by@gmail.com>
2018-10-06 16:57:25 +0300

    soluton

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.number = String(initialValue); 
   }
 
   add(number) {
-    // your implementation
+    this.number = this.number + '+' + number;    
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.number = this.number + '-' + number;    
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.number = this.number + '*' + number;    
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.number = this.number + '/' + number;    
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.number = this.number + '**' + number;
+    return this;
   }
+
+  valueOf() {    
+    return eval(this.number);
+  }
+
 }
 
 module.exports = SmartCalculator;
